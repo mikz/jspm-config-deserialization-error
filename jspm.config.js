5496dafe87cf465b61f282dcd67c64c1853c316c
Michal Cichra <michal@cichra.cz>
2016-02-07 14:47:52 +0100

    initial commit

---
+++ jspm.config.js
@@ -0,0 +1,35 @@
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json",
+    "github:*/*.json"
+  ],
+  globalEvaluationScope: false,
+  transpiler: "plugin-babel",
+
+  map: {
+    "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
+    "net": "github:jspm/nodelibs-net@0.2.0-alpha",
+    "plugin-babel": "npm:systemjs-plugin-babel@0.0.2",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "systemjs-hot-reloader": "github:capaj/systemjs-hot-reloader@0.5.5",
+    "tty": "github:jspm/nodelibs-tty@0.2.0-alpha",
+    "util": "github:jspm/nodelibs-util@0.2.0-alpha"
+  },
+
+  packages: {
+    "jspmtest": {},
+    "github:capaj/systemjs-hot-reloader@0.5.5": {
+      "map": {
+        "debug": "npm:debug@2.2.0",
+        "socket.io-client": "github:socketio/socket.io-client@1.4.5",
+        "weakee": "npm:weakee@1.0.0"
+      }
+    },
+    "npm:debug@2.2.0": {
+      "map": {
+        "ms": "npm:ms@0.7.1"
+      }
+    }
+  }
+});
